ab9ce031385b1266f9e56deb730c474d1bfdf68b
Remmzz <78427950+Remmzz@users.noreply.github.com>
2022-10-22 07:22:59 +0300

    Add files via upload

---
+++ terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+   cloud {
+    organization = "DarwinCorp"
+
+    workspaces {
+      name = "learn-terraform-for-each"
+    }
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.1.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+}
+
+provider "azurerm" {
+  features {}
+}
